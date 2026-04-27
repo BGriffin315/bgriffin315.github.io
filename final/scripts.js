3d305064a3b4ffe3d88a71daef35fe4a40e10d01
BGriffin315 <bgriffin1025@live.sunyjefferson.edu>
2026-04-26 20:35:45 -0400

    4/26/2026 8:35 p.m.

--- final/scripts.js
+++ final/scripts.js
@@ -49,15 +49,17 @@ function upLate() {
 
 upLate();
 
-document.querySelector('form').addEventListener("submit", function(e) {
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('form').addEventListener("submit", function(e) {
+    e.preventDefault();
 
-  const selected = document.querySelector('input[name="experienceLevel"]:checked');
-  const form = document.querySelector("form");
+    const selected = document.querySelector('input[name="experienceLevel"]:checked');
+    const form = document.querySelector("form");
 
-  if (selected) {
-    form.innerHTML = "<p>You can Join!</p><br>"
-  } else {
-    form.innerHTML = "<p>Select an option!</p><br>"
-  }
+    if (selected) {
+      form.innerHTML = "<p>You can Join!</p><br>"
+    } else {
+      form.innerHTML = "<p>Select an option!</p><br>"
+    }
+  })
 });
