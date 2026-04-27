5ea7afe6beee286bebbe8d5c6071eee2625686a5
BGriffin315 <bgriffin1025@live.sunyjefferson.edu>
2026-04-26 20:55:26 -0400

    4/26/2026 8:55 p.m.

--- final/joinForm.js
+++ final/joinForm.js
@@ -11,10 +11,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const selected = document.querySelector('input[name="experienceLevel"]:checked');
     const form = document.querySelector("form");
 
-    if (selected) {
-      form.innerHTML = "<p>You can Join!</p><br>"
+    if (selected.value === "Dean") {
+      form.innerHTML = `
+        <p>Hello, Dean, have you seen this yet?</p>
+        <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/watch?v=b0IWVeqvQGk"
+          title="FFXIV Evercold Teaser Trailer"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+      `;
     } else {
-      form.innerHTML = "<p>Select an option!</p><br>"
+      form.innerHTML = "<p>You can join!</p>";
     }
   })
 });
